cb1b8b16e24cfc3ca48c5d7e471b31dda4ced2ca
Osama Eldawebi <osama.eldawebi@gmail.com>
2019-09-05 22:51:28 +0200

    more setup stuff

--- bookmarket/views.py
+++ bookmarket/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -7,6 +15,53 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'bookmarket/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'bookmarket/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.authour = self.request.user
+        return super().form_valid(form)
+        """ SuccessUrl to home?"""
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.authour = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.authour:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.authour:
+            return True
+        return False
 
 
 def about(request):

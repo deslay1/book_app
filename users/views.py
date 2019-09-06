ebc56e2e90aa854f81f23425637500ae98d3b205
deslay1 <osama.eldawebi@gmail.com>
2019-09-06 13:00:28 +0200

    profile posts

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from bookmarket.models import Post
 
 
 def register(request):
@@ -45,7 +46,8 @@ def profile(request):
 
     context = {
         'u_form': u_form,
-        'p_form': p_form
+        'p_form': p_form,
+        'posts': Post.objects.all()
     }
 
     return render(request, 'users/profile.html', context)

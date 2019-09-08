87a9c224c726dd31a29036e162c5b3888c3f3383
tobiaskarlsson12 <tobiaskarlsson12356@gmail.com>
2019-09-08 12:27:56 +0200

    Minor changes. You can now view posts even if your not logged in.

--- users/views.py
+++ users/views.py
@@ -46,29 +46,14 @@ def profile(request):
 
 def profileUser(request, username):
 
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
+    
 
 
     inte = int(username, 10);
 
     context = {
         'userPost': inte,
-        'u_form': u_form,
-        'p_form': p_form,
+      
         'posts': Post.objects.all()
     }
 

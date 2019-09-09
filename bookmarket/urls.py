3dde093a2481db27b78ead9ec4531b281ef54dae
tobiaskarlsson12 <tobiaskarlsson12356@gmail.com>
2019-09-09 11:47:12 +0200

    yo

--- bookmarket/urls.py
+++ bookmarket/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('show_user_post', PostListView2.as_view(), name='app-show_post'),
+    path('show_user_post', PostListView.as_view(), name='app-show_post'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add-comment-to-post'),
 
 

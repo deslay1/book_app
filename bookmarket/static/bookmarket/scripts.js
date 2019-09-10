ac2afcd5078818c05e60e6f7dbff8ea5baf722bb
deslay1 <osama.eldawebi@gmail.com>
2019-09-10 17:57:49 +0200

    design home + tabs + authentication to comment

---
+++ bookmarket/static/bookmarket/scripts.js
@@ -0,0 +1,16 @@
+
+function Market(evt, market) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(market).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
--- bookmarket/urls.py
+++ bookmarket/urls.py
@@ -9,12 +9,11 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('show_user_post', PostListView2.as_view(), name='app-show_post'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add-comment-to-post'),
 
 
 
-    path('about/', views.about, name='app-about'),
+    path('about/', views.about, name='app-about')
 
 
 ]

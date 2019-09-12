2ba028eca454b3c20cb81e1e3996d9c04ca19709
tobiaskarlsson12 <tobiaskarlsson12356@gmail.com>
2019-09-12 19:30:33 +0200

    Before merge

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from django.db import models
-from phone_field import PhoneField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

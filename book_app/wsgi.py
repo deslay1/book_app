ff1067efb2d8b36241e0d4130269fc412c9b7017
deslay1 <osama.eldawebi@gmail.com>
2020-01-03 18:05:03 +0100

    reverted change in wsgi

--- book_app/wsgi.py
+++ book_app/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/Users/deslay/Documents/projects/book_app/book_app'
+""" path = '/Users/deslay/Documents/projects/book_app/book_app'
 if path not in sys.path:
-    sys.path.append(path)
+    sys.path.append(path) """
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_app.settings')
 

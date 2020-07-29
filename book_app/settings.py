bfa7c76525b7c75159bc577a040616998c3a3213
deslay1 <osama.eldawebi@gmail.com>
2020-07-29 23:37:31 +0200

    uncommented django_heroku

--- book_app/settings.py
+++ book_app/settings.py
@@ -1,5 +1,5 @@
 import os
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -179,4 +179,4 @@ MEDIA_URL = '/media/'
     os.path.join(BASE_DIR, 'additional'),
 ) """
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())

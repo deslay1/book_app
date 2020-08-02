0881d59da6a11ce47bd353f26dca91ba0ef2e970
deslay1 <osama.eldawebi@gmail.com>
2020-08-02 23:57:40 +0200

    temporary remove groups in users forms

--- users/forms.py
+++ users/forms.py
@@ -18,9 +18,7 @@ class UserRegisterForm(UserCreationForm):
                   'last_name', 'password1', 'password2']
 
     def getGroupNames():
-        groups = Group.objects.order_by(
-            "name").values_list('name', flat=True)
-        return zip(groups, groups)
+        return [("All", "All")]
 
     category = forms.ChoiceField(
         choices=getGroupNames(), label="What group do you belong to?")
